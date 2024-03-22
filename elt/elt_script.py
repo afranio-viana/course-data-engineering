72b5e49029aad0839247a0b6444c44011a0e73d1
afranio-viana <afranio.esquerdo.viana@gmail.com>
2024-03-21 23:05:37 -0400

    Adicionando cron

---
+++ elt/elt_script.py
@@ -0,0 +1,65 @@
+import subprocess
+import time
+
+def wait_for_postgres(host, max_retries=5, delay_seconds=5):
+    retries = 0
+    while retries < max_retries:
+        try:
+            result = subprocess.run(
+                ["pg_isready", "-h", host],check=True, capture_output=True, text=True)
+            if("accepting connections" in result.stdout):
+                print(f"Conectou, deu certo :)")
+                return True
+        except subprocess.CalledProcessError as e:
+            print(f"Erro de conexão como o PostgreSQL {e} :(")
+            retries = retries + 1
+            print(f"Tentando em {delay_seconds} segundos.... (Atenção {retries}/{max_retries})")
+            time.sleep(delay_seconds)
+    print(f"O número máximo de tentativas foi atingido")
+    return False
+
+if(not wait_for_postgres(host="source_postgres")):
+    exit(1)
+
+print("Iniciando Script ELT...")
+
+source_config = {
+    'dbname': 'source_db',
+    'user': 'postgres',
+    'password': 'secret',
+    'host': 'source_postgres'
+}
+
+destination_config = {
+    'dbname': 'destination_db',
+    'user': 'postgres',
+    'password': 'secret',
+    'host': 'destination_postgres'
+}
+
+dump_command = [
+    'pg_dump',
+    '-h', source_config['host'],
+    '-U', source_config['user'],
+    '-d', source_config['dbname'],
+    '-f', 'data_dump.sql',
+    '-w'
+]
+
+subprocess_env = dict(PGPASSWORD=source_config['password'])
+
+subprocess.run(dump_command, env=subprocess_env, check=True)
+
+load_command = [
+    'psql',
+    '-h', destination_config['host'],
+    '-U', destination_config['user'],
+    '-d', destination_config['dbname'],
+    '-a','-f', 'data_dump.sql'
+]
+
+subprocess_env = dict(PGPASSWORD=destination_config['password'])
+
+subprocess.run(load_command, env=subprocess_env,check=True)
+
+print(f"Fim do Script ELT :)")
